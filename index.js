aec058fcfec125e84e5a2e13f79f9ffff5072e4b
PatrickMorehead <patrick.a.morehead@gmail.com>
2024-01-31 11:04:58 -0600

    API requests working

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+$(document).ready(function () {
 
+    $.ajax({
+        type: 'GET',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1157',
+        dataType: 'json',
+        success: function (response, textStatus) {
+            console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+        }
+    })
+})
 
