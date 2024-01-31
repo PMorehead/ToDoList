25107be00b6d4ffc165ccf724a32314c35c3a2e1
PatrickMorehead <patrick.a.morehead@gmail.com>
2024-01-31 14:39:54 -0600

    Added ability to delete tasks

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             success: function (response, textStatus) {
                 $('#todo-list').empty();
                 response.tasks.forEach(function (task) {
-                    $('#todo-list').append('<p>' + task.content + '</p>');
+                    $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
                 })
             },
             error: function (request, textStatus, errorMessage) {
@@ -35,13 +35,29 @@ $(document).ready(function () {
         });
     }
 
-    
+    var deleteTask = function (id) {
+        $.ajax({
+            type: 'DELETE',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/' + id +'?api_key=1157',
+            success: function (response, textStatus) {
+                getAndDisplayAllTasks();
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
 
     $('#create-task').on('submit', function (e) {
         e.preventDefault();
         createTask();
     })
+
+    $(document).on('click', '.delete', function () {
+        deleteTask($(this).data('id'));
+    })
     
     getAndDisplayAllTasks();
+
 });
 
