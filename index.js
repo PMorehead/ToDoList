7f0049d31a0e29ed365f84762e9623e741f8e519
PatrickMorehead <patrick.a.morehead@gmail.com>
2024-01-31 13:42:01 -0600

    Added ability to retrieve data and add it to the DOM

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
         url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1157',
         dataType: 'json',
         success: function (response, textStatus) {
-            console.log(response);
+            response.tasks.forEach(function (task) {
+                $('#todo-list').append('<p>' + task.content + '</p>');
+            })
         },
         error: function (request, textStatus, errorMessage) {
             console.log(errorMessage);
         }
-    })
+    });
 })
 
