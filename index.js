336e7e34b946d67c9bc4a0ad0cce28abf355a695
PatrickMorehead <patrick.a.morehead@gmail.com>
2024-01-31 14:24:54 -0600

    Added ability for user to create new items

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
 
+    var createTask = function () {
+        $.ajax({
+            type: 'POST',
+            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1157',
+            dataType: 'json',
+            data: JSON.stringify({
+                task: {
+                    content: $('#new-task-content').val()
+                }
+            }),
+            success: function (response, textStatus) {
+                console.log(response);
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
     $.ajax({
         type: 'GET',
         url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1157',
@@ -13,5 +32,11 @@ $(document).ready(function () {
             console.log(errorMessage);
         }
     });
-})
+
+    $('#create-task').on('submit', function (e) {
+        e.preventDefault();
+        createTask();
+    })
+    
+});
 
