6c8d676e2ed8456b82d7b528cd82c36f2057313a
PatrickMorehead <patrick.a.morehead@gmail.com>
2024-01-31 14:51:07 -0600

    Added ability to mark tasks complete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             success: function (response, textStatus) {
                 $('#todo-list').empty();
                 response.tasks.forEach(function (task) {
-                    $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+                    $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked': '') + '>');
                 })
             },
             error: function (request, textStatus, errorMessage) {
@@ -48,6 +48,20 @@ $(document).ready(function () {
         });
     }
 
+    var markTaskComplete = function (id) {
+        $.ajax({
+            type: 'PUT',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/' + id +'?api_key=1157',
+            dataType: 'json',
+            success: function (response, textStatus) {
+                console.log(response);
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
     $('#create-task').on('submit', function (e) {
         e.preventDefault();
         createTask();
@@ -56,6 +70,10 @@ $(document).ready(function () {
     $(document).on('click', '.delete', function () {
         deleteTask($(this).data('id'));
     })
+
+    $(document).on('change', '.mark-complete', function () {
+        markTaskComplete($(this).data('id'));
+    });
     
     getAndDisplayAllTasks();
 
